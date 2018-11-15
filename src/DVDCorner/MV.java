69401445a784b4590682a550c5d825133d963c2a
Alex Yip <yiucheung1999@gmail.com>
2018-11-15 19:05:06 +0800

    bg

---
+++ src/DVDCorner/MV.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DVDCorner;
+
+/**
+ *
+ * @author YipYi
+ */
+public class MV extends DVD{
+
+    private String singer;
+
+    public MV(int dvdID, String title, int length, int numAvailable, String singer) {
+        super(dvdID, title, length, numAvailable);
+        this.singer = singer;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    @Override
+    public String toString() {
+        return "MV{" + "singer=" + singer + '}';
+    }
+}
--- src/DVDCorner/Main.java
+++ src/DVDCorner/Main.java
@@ -5,6 +5,9 @@
  */
 package DVDCorner;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author YipYi
@@ -15,7 +18,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+        int command;
+        String[] factoryCmd = {"DVDCorner.CreateDVDFactory", "DVDCorner.ShowDVDFactory", "DVDCorner.DonateDVDFactory", "DVDCorner.LendDVDFactory", "DVDCorner.ReturnDVDFactory"};
+
+        boolean cont = true;
+        while (cont) {
+            try {
+                while (cont) {
+                    System.out.println("DVD Record System");
+                    System.out.println("Please enter command: [c | s | a | l | g | u | r | d | x]");
+                    System.out.println("c = create DVD, s = show DVD, a = accept donation of DVD, l = lend out a DVD, g = get back a returned DVD, u = undo, r = redo, d = display undo/redo list, x = exit system\n");
+                    command = Integer.parseInt(br.readLine());
+                    
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
-    
+
 }
